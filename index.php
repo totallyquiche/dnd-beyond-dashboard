76db29ff45c27e1df31706ecb4ff9e91ebcda9e7
Brian Dady <bndady@gmail.com>
2020-05-03 21:57:48 -0500

    Moves section into separate PHP file

--- index.php
+++ index.php
@@ -24,15 +24,7 @@
     <body>
         <div class="container bg-dark">
             <div class="container mt-4 mb-4 mt-lg-5 mb-lg-5">
-                <div id="logo-row" class="row pt-4 pb-4">
-                    <div class="col-4 col-md-5"></div>
-                    <div class="col-4 col-md-2 text-center">
-                        <a href="https://www.dndbeyond.com" title="Go to the D&amp;D Beyond website" target="_BLANK">
-                            <img src="assets/images/die.png" class="img-fluid" alt="A red D20 die"/>
-                        </a>
-                    </div>
-                    <div class="col-4 col-md-5"></div>
-                </div>
+                <?php include('sections/logo.php'); ?>
             </div>
         </div>
 
---
+++ sections/logo.php
@@ -0,0 +1,9 @@
+<div id="logo-row" class="row pt-4 pb-4">
+    <div class="col-4 col-md-5"></div>
+    <div class="col-4 col-md-2 text-center">
+        <a href="https://www.dndbeyond.com" title="Go to the D&amp;D Beyond website" target="_BLANK">
+            <img src="assets/images/die.png" class="img-fluid" alt="A red D20 die"/>
+        </a>
+    </div>
+    <div class="col-4 col-md-5"></div>
+</div>
