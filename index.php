95bb5aae150d09f687da87d020273ce51dcfb941
Brian Dady <bndady@gmail.com>
2020-05-03 22:00:19 -0500

    Adds search section

--- index.php
+++ index.php
@@ -25,11 +25,42 @@
         <div class="container bg-dark">
             <div class="container mt-4 mb-4 mt-lg-5 mb-lg-5">
                 <?php include('sections/logo.php'); ?>
+                <?php include('sections/search.php'); ?>
             </div>
         </div>
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <script>
+            $(function () {
+                var searchForm = $("#search-form");
+                var searchBar = $("#search-bar");
+
+                searchBar.focus();
+
+                searchForm.on("click", ".btn", function (event) {
+                    let button = $(event.target);
+                    let resource = button.data("resource");
+                    let paramName = button.data("param-name");
+
+                    searchForm.find(".btn").removeClass("bg-danger active");
+                    button.addClass("bg-danger active");
+                    searchForm.attr("action", "https://www.dndbeyond.com/" + resource + "?");
+                    searchBar.attr("name", paramName);
+                    searchBar.focus();
+                });
+
+                searchForm.on("submit", function (event) {
+                    event.preventDefault();
+
+                    this.submit();
+                    this.reset();
+                });
+
+                searchForm.find(".btn").first().trigger("click");
+            });
+        </script>
     </body>
 </html>
---
+++ sections/search.php
@@ -0,0 +1,18 @@
+<div id="search-row" class="row pb-4 pb-lg-5">
+    <div class="col-0 col-lg-1"></div>
+    <div class="col-12 col-lg-10">
+        <form id="search-form" action="" method="GET" target="_BLANK">
+            <div class="input-group input-group-sm">
+                <div class="input-group-prepend rounded-0 d-none d-md-flex">
+                    <button class="btn btn-outline-secondary rounded-0 active" type="button" data-resource="search" data-param-name="q">All</button>
+                    <button class="btn btn-outline-secondary rounded-0" type="button" data-resource="spells" data-param-name="filter-search">Spells</button>
+                    <button class="btn btn-outline-secondary rounded-0" type="button" data-resource="monsters" data-param-name="filter-search">Monsters</button>
+                    <button class="btn btn-outline-secondary rounded-0" type="button" data-resource="equipment" data-param-name="filter-search">Equipment</button>
+                    <button class="btn btn-outline-secondary rounded-0" type="button" data-resource="magic-items" data-param-name="filter-search">Magic Items</button>
+                </div>
+                <input id="search-bar" name="" type="text" value=""0 class="form-control rounded-0" placeholder="Search here"/>
+            </div>
+        </form>
+    </div>
+    <div class="col-0 col-lg-1"></div>
+</div>
