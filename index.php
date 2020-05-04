8edb84e4cac3304b736fd547e976a89e0b671db6
Brian Dady <bndady@gmail.com>
2020-05-03 21:43:26 -0500

    Creates Bootstrap shell with logo, background, and container elements

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Quickly view common D&D 5e mechanics and search D&D Beyond">
+
+        <title>D&amp;D Beyond Dashboard</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto+Mono&display=swap" rel="stylesheet">
+
+        <style type="text/css">
+            body {
+                background-image: url('assets/images/background.jpg');
+                background-attachment: fixed;
+                background-size: cover;
+                font-family: "Roboto Mono";
+            }
+        </style>
+
+    </head>
+    <body>
+        <div class="container bg-dark">
+            <div class="container mt-4 mb-4 mt-lg-5 mb-lg-5">
+                <div id="logo-row" class="row pt-4 pb-4">
+                    <div class="col-4 col-md-5"></div>
+                    <div class="col-4 col-md-2 text-center">
+                        <a href="https://www.dndbeyond.com" title="Go to the D&amp;D Beyond website" target="_BLANK">
+                            <img src="assets/images/die.png" class="img-fluid" alt="A red D20 die"/>
+                        </a>
+                    </div>
+                    <div class="col-4 col-md-5"></div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
